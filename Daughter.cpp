802983b9a22879ab3f3d19c2813abbc4f0cda89d
Max <162749285+Cyber-Limon@users.noreply.github.com>
2024-08-29 20:31:01 +0500

    Add files via upload

---
+++ Daughter.cpp
@@ -0,0 +1,34 @@
+﻿# include <iostream>
+
+using namespace std;
+
+int factorial(int n) {
+
+	if (n < 0) {
+		return 0;
+	}
+	else if (n == 0) {
+		return 1;
+	}
+	else {
+		return n * factorial(n - 1);
+	}
+}
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+
+	int n;
+	int res;
+
+	cout << "Введите число ";
+	cin >> n;
+	res = factorial(n);
+	if (res > 0) {
+		cout << "Ответ " << res;
+	}
+	else {
+		cout << "Ошибка ";
+	}
+}
---
+++ MainProc.cpp
@@ -0,0 +1,18 @@
+﻿#include <windows.h> 
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+	
+	STARTUPINFO StartupInfo;
+	ZeroMemory(&StartupInfo, sizeof(StartupInfo));
+	StartupInfo.cb = sizeof(StartupInfo);
+	PROCESS_INFORMATION ProcInfo;
+	ZeroMemory(&ProcInfo, sizeof(ProcInfo));
+	wchar_t AppName[] = L"C:\\Users\\Максим\\source\\repos\\Daughter\\x64\\Debug\\Daughter.exe";
+	if (!CreateProcess(AppName, NULL, NULL, NULL, FALSE, 0, NULL, NULL, &StartupInfo, &ProcInfo)) return 0;
+	WaitForSingleObject(ProcInfo.hProcess, INFINITE);
+	CloseHandle(ProcInfo.hProcess);
+	CloseHandle(ProcInfo.hThread);
+}
